3137139329c4007bff4415837d4ca319d338f24a
Simranjeetaws <simranjeetkaurtind@gmail.com>
2024-11-04 22:58:48 +0530

    Update User activity for the past 30 days.sql

--- User activity for the past 30 days.sql
+++ User activity for the past 30 days.sql
@@ -1,5 +1,4 @@
-
 select activity_date as day , count(distinct user_id) as active_users
 from activity
-WHERE DATEDIFF("2019-07-27", activity_date) < 30 
+WHERE activity_date BETWEEN '2019-06-28' AND  '2019-07-27'
 group by activity_date
